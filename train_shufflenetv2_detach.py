90b68a6903d2bef4c358e295d88b25e6fc6daf54
yingjun.wang <beichen2012@qq.com>
2018-12-07 10:47:31 +0800

    添加恢复训练的代码，调度按epoch变化lr

--- train_shufflenetv2_detach.py
+++ train_shufflenetv2_detach.py
@@ -17,10 +17,15 @@ import numpy as np
 import random
 from shufflenetv2_ssd_detach import *
 from utils import Logger
+from utils.torchutil import SaveCheckPoint
 # from utils import make_dot
 if not os.path.exists("./log"):
     os.mkdir("./log")
-log = Logger("./log/{}.log".format(__file__.split('/')[-1], level='debug')).logger
+log = Logger("./log/{}_{}.log".format(__file__.split('/')[-1],
+                                             time.strftime("%Y%m%d-%H%M%S"), time.localtime), level='debug').logger
+
+pre_checkpoint = "./models/shufflenetv2_ssd_detach_20181201_iter_260000.pkl"
+resume = False
 
 USE_CUDA = True
 GPU_ID = [0]
@@ -29,21 +34,22 @@ os.environ["CUDA_VISIBLE_DEVICES"] = ",".join([str(i) for i in GPU_ID])
 device = torch.device("cuda" if torch.cuda.is_available() and USE_CUDA else "cpu")
 
 train_batch = 32
-display = 20
+display = 50
 
-base_lr = 0.01
+base_lr = 0.001
+clip_grad = 20.0
 momentum = 0.9
 gamma = 0.1
 weight_decay = 0.0005
-stepsize = [80000, 150000, 180000, 200000, 220000]
-max_iter = 230000
+stepsize = [150, 500, 700]
+max_epoch = 800
 
 save_interval = 10000
 
 save_dir = "./models"
 if not os.path.exists(save_dir):
     os.mkdir(save_dir)
-save_prefix = save_dir + "/shufflenetv2_ssd_detach_20181201"
+save_prefix = save_dir + "/shufflenetv2_ssd_detach_20181207"
 DATASET_ROOT = "~/dataset/VOCdevkit"
 DATASET_ROOT = os.path.expanduser(DATASET_ROOT)
 
@@ -53,7 +59,9 @@ def _worker_init_fn_():
     np_seed = torch_seed // 2**32-1
     random.seed(torch_seed)
     np.random.seed(np_seed)
+
 def train():
+    start_epoch = 0
     cfg = voc_shufflenetv2
 
     dataset = VOCDetection(root=DATASET_ROOT,
@@ -77,31 +85,37 @@ def train():
     # criterion
     criterion = MultiBoxLoss(cfg['num_classes'], 0.5, True, 0, True, 3, 0.5,
                              False, device)
-
-
-    if USE_CUDA:
-        net = torch.nn.DataParallel(ssd_net)
-        cudnn.benchmark = True
-    net.to(device)
-    priorboxes = priorboxes.to(device)
-    criterion.to(device)
-
     optimizer = optim.SGD(net.parameters(), lr=base_lr, momentum=momentum,
                           weight_decay=weight_decay)
     scheduler = optim.lr_scheduler.MultiStepLR(optimizer, stepsize, gamma)
 
 
-    num_epoch = max_iter // train_batch + 1
+    if USE_CUDA:
+        net = torch.nn.DataParallel(net)
+        cudnn.benchmark = True
+    if pre_checkpoint:
+        cp = torch.load(pre_checkpoint)
+        net.load_state_dict(cp['weights'])
+        # net.load_state_dict(cp)
+        log.info("=> load state dict from {}...".format(pre_checkpoint))
+        if resume:
+            optimizer.load_state_dict(cp['optimizer'])
+            scheduler.load_state_dict(cp['scheduler'])
+            start_epoch = cp['epoch']
+            log.info("=> resume from epoch: {}, now the lr is: {}".format(start_epoch, optimizer.param_groups[0]['lr']))
+
+    net.to(device)
+    priorboxes = priorboxes.to(device)
+    criterion.to(device)
+
     k = 0
     loc_loss = 0
     conf_loss = 0
-    epoch = 0
     loss = 0
-    for i in range(0, num_epoch):
-        net.train()
-
+    for epoch in range(start_epoch, max_epoch + 1):
         # one eopch
         for batch_idx, (images, targets) in enumerate(data_loader):
+            net.train()
             images = images.to(device)
             targets = [i.to(device) for i in targets]
 
@@ -117,8 +131,6 @@ def train():
             loss.backward()
             torch.nn.utils.clip_grad_norm_(net.parameters(), 20.0)
             optimizer.step()
-            scheduler.step()
-
             t1 = time.time()
             loc_loss += loss_l.item()
             conf_loss += loss_c.item()
@@ -133,15 +145,16 @@ def train():
                     t1-t0))
             if k % save_interval == 0:
                 path = save_prefix + "_iter_{}.pkl".format(k)
-                torch.save(net.to('cpu').state_dict(), path)
-                net.to(device)
-                log.info("save model: {}".format(path))
+                SaveCheckPoint(path, net, optimizer, scheduler, epoch)
+                log.info("=> save model: {}".format(path))
             k += 1
-        log.info('epoch: {}, lr: {}, loss is: {}'.format(i, optimizer.param_groups[0]['lr'], loss.item()))
+        log.info('==> epoch: {}, lr: {}, loss is: {}'.format(epoch, optimizer.param_groups[0]['lr'], loss.item()))
+        scheduler.step()
+
     log.info("optimize done...")
     path = save_prefix + "_final.pkl"
-    torch.save(net.to('cpu').state_dict(), path)
-    log.info("save model: {} ...".format(path))
+    SaveCheckPoint(path, net, optimizer, scheduler, max_epoch)
+    log.info("=> save model: {} ...".format(path))
 
 
 if __name__ == '__main__':
---
+++ utils/torchutil.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+
+import torch
+
+
+def SaveCheckPoint(path, net, optimizer, scheduler, epoch):
+    checkpoint = {
+        'weights': net.state_dict(),
+        'optimizer': optimizer.state_dict(),
+        'scheduler': scheduler.state_dict(),
+        'epoch': epoch
+    }
+    torch.save(checkpoint, path)
+
